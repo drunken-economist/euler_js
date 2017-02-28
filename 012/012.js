62ca2602522144514091273fdb4e79f484a05f40
drunken-economist <justin@reddit.com>
2017-02-27 23:51:40 -0600

    solved 12 in O(n^2)

---
+++ 012/012.js
@@ -0,0 +1,34 @@
+function getNumFactors(dividend){
+	var divisors = 2;
+	for(i = 2; i < dividend; i++){
+		if (dividend % i == 0){
+			divisors++;
+		}
+	}
+	return divisors;
+}
+
+
+function triangleWithFactors(numFactors){
+	if(numFactors == 1){
+		return 1;
+	}
+	if(numFactors < 0 || numFactors % 1 != 0){
+		return "knock it off";
+	}
+	var thisNum = 2;
+	var triangleSum = 1;
+	var max = 0;
+	while (true){
+		triangleSum += thisNum;
+		var howMany = getNumFactors(triangleSum);
+		if(howMany > max){
+			max = howMany;
+			console.log(howMany);
+		}
+		if (howMany > numFactors){
+			return triangleSum;
+		}
+		thisNum++;
+	}
+}
